f63d5d8c3ddb054a6a766e061f157fd331541876
Spencer Clark <spencerkclark@gmail.com>
2018-11-27 14:35:55 -0500

    Add optional custom local emacs configuration file

--- emacs/emacs.d/init.el
+++ emacs/emacs.d/init.el
@@ -6,3 +6,8 @@
 (require 'pallet)
 
 (org-babel-load-file "~/.emacs.d/configuration.org")
+
+;; Allow for an optional local configuration file to
+;; add or override my universal settings.
+(if (file-exists-p "~/.emacs_local.org")
+    (org-babel-load-file "~/.emacs_local.org"))
